1b1498bf038ed4de343d9ef4cc4b4032b3d5e284
Peter Wittich <wittich@cornell.edu>
2015-02-28 13:53:00 -0500

    fixed thrust

--- cuda/nbody.cu
+++ cuda/nbody.cu
@@ -9,7 +9,17 @@
 
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
+#include <thrust/sequence.h>
 
+#if defined(__CUDACC__) // NVCC
+#define MY_ALIGN(n) __align__(n)
+#elif defined(__GNUC__) // GCC
+#define MY_ALIGN(n) __attribute__((aligned(n)))
+#elif defined(_MSC_VER) // MSVC
+#define MY_ALIGN(n) __declspec(align(n))
+#else
+  #error "Please provide a definition for MY_ALIGN macro for your host compiler!"
+#endif
 
 float4
 __device__ __host__ operator*(const float4 a, const float4 b)
@@ -43,73 +53,10 @@ __device__ __host__ operator+(const float4 a, const float4 b)
 
 using namespace thrust;
 
-class float4_t {
- public:
-  float x,y,z,w;
-  float4_t operator+=(const float4_t & rhs ) {
-    this->x += rhs.x;
-    this->y += rhs.y;
-    this->z += rhs.z;
-    this->w += rhs.w;
-    return *this ;
-  }
-   float4_t operator+( const float4_t & rhs ) const {
-    float4_t tmp = *this;
-    tmp += rhs;
-    return tmp;
-  }
-  float4_t operator-=(const float4_t & rhs ) {
-    this->x -= rhs.x;
-    this->y -= rhs.y;
-    this->z -= rhs.z;
-    this->w -= rhs.w;
-    return *this ;
-  }
-  float4_t operator-( const float4_t & rhs )  const {
-    float4_t tmp = *this;
-    tmp -= rhs;
-    return tmp;
-  }
-  float4_t operator*=(const float4_t & rhs ) {
-    this->x *= rhs.x;
-    this->y *= rhs.y;
-    this->z *= rhs.z;
-    this->w *= rhs.w;
-    return *this ;
-  }
-  float4_t operator*(const float4_t & rhs ) const {
-    float4_t tmp = *this;
-    tmp *= rhs;
-    return tmp;
-  }
 
-  float4_t operator*=(const float & rhs) {
-    this->x *= rhs;
-    this->y *= rhs;
-    this->z *= rhs;
-    this->w *= rhs;
-    return *this ;
-  }
-  float4_t operator*(const float & rhs) const {
-    float4_t tmp = *this;
-    tmp *= rhs;
-    return tmp;
-  }
-    
- float4_t(float ax, float ay, float az, float aw) :
-  x(ax), y(ay), z(az), w(aw)
-  {}
-};
-
-float4_t operator*(const float & lhs, const float4_t & rhs ) {
-  float4_t tmp=rhs;
-  tmp *= lhs;
-  return tmp;
-}
-
-__device__ __host__
+__device__ __host__ 
 void update_particle(const size_t i, const size_t nparticle, 
-		     const float4* pos_old,
+		     const float4 * pos_old,
 		     float4 * pos_new, 
 		     float4 * vel)
 {
@@ -138,69 +85,67 @@ void update_particle(const size_t i, const size_t nparticle,
 }
 
 // SOA - sorta?
-struct wrapper_t {
-  //thrust::device_vector<float4> d_pos1, d_pos2, d_vel;
-  float4 *d_pos1, *d_pos2, *d_vel;
+struct MY_ALIGN(16) wrapper_t  {
   int toggle; // pos1->pos2 or pos2->pos1
   size_t nparticle;
+  float4 *pos1, *vel, *pos2;
 };
 
 
-class functor_thrust {
+class MY_ALIGN(16) functor_thrust {
 private:
-  struct wrapper_t *_wrapper;
+  const struct wrapper_t _wrapper;
 public:
-   functor_thrust( struct wrapper_t *w ):
+  __host__ __device__
+   functor_thrust( struct wrapper_t w ):
      _wrapper(w)
   {}
   
-  __device__ 
+  __device__ __host__ 
   void operator()(const int i) const 
   {
-    if ( _wrapper->toggle ) 
-      update_particle(i, _wrapper->nparticle, _wrapper->d_pos2, _wrapper->d_pos1, _wrapper->d_vel);
+    if ( _wrapper.toggle ) 
+      update_particle(i, _wrapper.nparticle, _wrapper.pos1, _wrapper.pos2, _wrapper.vel);
     else
-      update_particle(i, _wrapper->nparticle, _wrapper->d_pos1, _wrapper->d_pos2, _wrapper->d_vel);
+      update_particle(i, _wrapper.nparticle, _wrapper.pos2, _wrapper.pos1, _wrapper.vel);
       
   }
 
 };
 
-#ifdef TBB
-class functor_tbb {
-private:
-   const float4 * p_pos_old;
-   float4 * p_pos_new;
-   float4 * p_vel; 
-   int nparticle;
-public:
-   functor_tbb(   const float4 * pos_old,
-	      float4 * pos_new,
-	      float4 * vel,
-	      int n):
-      p_pos_old(pos_old),
-      p_pos_new(pos_new),
-      p_vel(vel),
-      nparticle(n)
-   {}
-
-   
-   void operator()(const blocked_range<size_t> & r) const 
-   {
-      for ( size_t i = r.begin(); i != r.end(); ++i ) {
-	 update_particle(i, nparticle, p_pos_old, p_pos_new, p_vel);
-      }
-   }
-
-};
-#endif // TBB
-
+#define CUDA_SAFE_CALL(call) call
 
 int main()
 {
+
+
+  int num_devices, device;
+  CUDA_SAFE_CALL(cudaGetDeviceCount(&num_devices));
+  printf("This many devices: %d\n", num_devices);
+  int max_multiprocessors = -1, max_device = -1;
+  cudaDeviceProp best_prop;
+  for ( device = 0; device < num_devices; ++device ) {
+    cudaDeviceProp properties;
+    CUDA_SAFE_CALL(cudaGetDeviceProperties(&properties, device));
+    if ( max_multiprocessors < properties.multiProcessorCount ) {
+      max_multiprocessors = properties.multiProcessorCount;
+      max_device = device;
+      best_prop = properties;
+    }
+  }
+  if ( max_device >=0 )
+    cudaSetDevice(max_device);
+  else  {
+    printf("problem finding a good device! aborting.\n");
+    return 1;
+  }
+  printf("# Running on device %d (name %s)\n", max_device, best_prop.name);
+
+
+
   //uint64_t        t0, t1, t2;
-  int nparticle = 2*8192; /* MUST be a nice power of two for simplicity */
-  const int nstep = 5;
+  int nparticle = 8; /* MUST be a nice power of two for simplicity */
+  const int nstep = 3;
   //int nburst = 20; /* MUST divide the value of nstep without remainder */
   //int nthread = 64; /* chosen for ATI Radeon HD 5870 */
 
@@ -208,51 +153,85 @@ int main()
   const float eps = 0.0001;
 
 
+  thrust::device_vector<int> d_ivals(nparticle);
+  thrust::host_vector<int> h_ivals(nparticle);
+  thrust::sequence(d_ivals.begin(), d_ivals.end()); // 0..nparticle-1
+  printf("copying from GPU\n");
+  h_ivals = d_ivals;
+  //return 0;
+
   thrust::host_vector<float4> h_pos1(nparticle), h_pos2(nparticle), h_vel(nparticle);
   thrust::device_vector<float4> d_pos1(nparticle), d_pos2(nparticle), d_vel(nparticle);
-  thrust::device_vector<int> d_ivals(nparticle);
-  thrust::sequence(d_ivals.begin(), d_ivals.end()); // 0..nparticle-1
-  
+
   
   const float4 dt0 = make_float4(dt,dt,dt,0.0f);
 
+  printf("making particles .... \n");
+  srand(1232773);
   for ( int i = 0; i < nparticle; ++i ){
-    
-    h_pos1.push_back(make_float4((float)rand()/RAND_MAX * 100. - 50.,
-				       (float)rand()/RAND_MAX * 100. - 50.,
-				       (float)rand()/RAND_MAX * 100. - 50.,
-				       (float)rand()/RAND_MAX * 10. // mass
-				       ));
-    h_pos2.push_back(make_float4(0.f,0.f,0.f,0.f));
-    h_vel. push_back(make_float4(0.f,0.f,0.f,0.f));
+    h_pos1[i] = make_float4(100.*rand()/RAND_MAX  - 50.,
+			    100.*rand()/RAND_MAX  - 50.,
+			    100.*rand()/RAND_MAX - 50.,
+			    10.*rand()/RAND_MAX // mass
+			    );
+    h_pos2[i] = make_float4(0.f,0.f,0.f,0.f);
+    h_vel[i]  = make_float4(0.f,0.f,0.f,0.f);
+  }
+  for ( int i = 0; i < nparticle; ++i ) {
+    printf("%d\t%f\n", i,h_pos1[i]);
   }
 
   struct wrapper_t wrapper;
+  struct wrapper_t h_wrapper;
   // copy to GPU
+  printf("copying to GPU .... 1\n");
   d_pos1 = h_pos1;
+  printf("copying to GPU .... 2\n");
   d_pos2 = h_pos2;
+  printf("copying to GPU .... 3\n");
   d_vel  = h_vel;
-  wrapper.d_pos1 = thrust::raw_pointer_cast(d_pos1.data());
-  wrapper.d_pos2 = thrust::raw_pointer_cast(d_pos2.data());
-  wrapper.d_vel  = thrust::raw_pointer_cast(d_vel .data());
-  
+  printf("done copying to GPU .... \n");
+  wrapper.pos1 = thrust::raw_pointer_cast(d_pos1.data());
+  wrapper.pos2 = thrust::raw_pointer_cast(d_pos2.data());
+  wrapper.vel  = thrust::raw_pointer_cast(d_vel .data());
+  wrapper.nparticle = nparticle;
+  wrapper.toggle = 0;
+
+  h_wrapper.pos1 = thrust::raw_pointer_cast(h_pos1.data());
+  h_wrapper.pos2 = thrust::raw_pointer_cast(h_pos2.data());
+  h_wrapper.vel  = thrust::raw_pointer_cast(h_vel .data());
+  h_wrapper.nparticle = nparticle;
+  h_wrapper.toggle = 0;
+
+
 
   int which = 8;
 
   
   // loop over time steps
+  printf("Starting loop \n");
   for ( int istep = 0; istep<nstep; ++istep ) {
-    printf("istep = %d,",istep);
-    int toggle;  // pos1->pos2 or pos2->pos1
-    if ( istep%2==1 ) {
+    printf("istep = %d,\n",istep);
+    if ( istep%2==0 ) {
       wrapper.toggle = 1;
     }
     else {
       wrapper.toggle = 0;
     }
-    thrust::for_each(d_ivals.begin(), d_ivals.end(), functor_thrust(&wrapper));
+    try {
+      thrust::for_each(d_ivals.begin(), d_ivals.end(), functor_thrust(wrapper));
+    }
+    catch(thrust::system_error &e) {
+      printf("error: %s\n", e.what());
+    }
     
-    //std::for_each(parts.begin(), parts.end(), functor_serial(pos_old, pos_new, &vel,nparticle));
+    // if ( istep%2==0 ) {
+    //   h_wrapper.toggle = 1;
+    // }
+    // else {
+    //   h_wrapper.toggle = 0;
+    // }
+    // std::for_each(h_ivals.begin(), h_ivals.end(), functor_thrust(h_wrapper));
     
     // tbb::parallel_for(blocked_range<size_t>(0,nparticle),
     //  		      functor_tbb(pos_old, pos_new, &vel,nparticle));
