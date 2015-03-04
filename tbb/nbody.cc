3cd2d00d510c784c19fc3c6d58a409a59b8cbad7
Peter Wittich <wittich@cornell.edu>
2015-03-04 10:27:13 -0500

    working on soa version (not done)

--- tbb/nbody.cc
+++ tbb/nbody.cc
@@ -12,7 +12,7 @@
 using namespace tbb;
 
 class float4_t {
- public:
+public:
   float x,y,z,w;
   float4_t operator+=(const float4_t & rhs ) {
     this->x += rhs.x;
@@ -21,7 +21,7 @@ class float4_t {
     this->w += rhs.w;
     return *this ;
   }
-   float4_t operator+( const float4_t & rhs ) const {
+  float4_t operator+( const float4_t & rhs ) const {
     float4_t tmp = *this;
     tmp += rhs;
     return tmp;
@@ -64,8 +64,8 @@ class float4_t {
     return tmp;
   }
     
- float4_t(float ax, float ay, float az, float aw) :
-  x(ax), y(ay), z(az), w(aw)
+  float4_t(float ax, float ay, float az, float aw) :
+    x(ax), y(ay), z(az), w(aw)
   {}
 };
 
@@ -87,11 +87,11 @@ void update_particle(const size_t i, const size_t nparticle,
   float4_t v = (*vel)[i];
   float4_t a = float4_t(0.0f,0.0f,0.0f,0.0f);
   for(int j=0; j<nparticle; j++) { // inner loop over particles
-     const float4_t p2 = (*pos_old)[j]; //Read a cached particle position */
-     float4_t d = p2 - p;
-     const float invr = 1./sqrt(d.x*d.x + d.y*d.y + d.z*d.z + eps);
-     const float f = p2.w*invr*invr*invr;
-     a += f*d; // Accumulate acceleration 
+    const float4_t p2 = (*pos_old)[j]; //Read a cached particle position */
+    float4_t d = p2 - p;
+    const float invr = 1./sqrt(d.x*d.x + d.y*d.y + d.z*d.z + eps);
+    const float f = p2.w*invr*invr*invr;
+    a += f*d; // Accumulate acceleration 
   }
   
   p += dt0*v + 0.5f*dt0*dt0*a;
@@ -106,53 +106,53 @@ typedef std::vector<float4_t> float4_ts;
 
 class functor_serial {
 private:
-   const float4_ts * p_pos_old;
-   float4_ts * p_pos_new;
-   float4_ts * p_vel; 
-   int nparticle;
+  const float4_ts * p_pos_old;
+  float4_ts * p_pos_new;
+  float4_ts * p_vel; 
+  int nparticle;
 public:
-   functor_serial(   const float4_ts * pos_old,
-	      float4_ts * pos_new,
-	      float4_ts * vel,
-	      int n):
-      p_pos_old(pos_old),
-      p_pos_new(pos_new),
-      p_vel(vel),
-      nparticle(n)
-   {}
+  functor_serial(   const float4_ts * pos_old,
+		    float4_ts * pos_new,
+		    float4_ts * vel,
+		    int n):
+    p_pos_old(pos_old),
+    p_pos_new(pos_new),
+    p_vel(vel),
+    nparticle(n)
+  {}
 
    
-   void operator()(const int i) const 
-   {
-     update_particle(i, nparticle, p_pos_old, p_pos_new, p_vel);
-   }
+  void operator()(const int i) const 
+  {
+    update_particle(i, nparticle, p_pos_old, p_pos_new, p_vel);
+  }
 
 };
 
 class functor_tbb {
 private:
-   const float4_ts * p_pos_old;
-   float4_ts * p_pos_new;
-   float4_ts * p_vel; 
-   int nparticle;
+  const float4_ts * p_pos_old;
+  float4_ts * p_pos_new;
+  float4_ts * p_vel; 
+  int nparticle;
 public:
-   functor_tbb(   const float4_ts * pos_old,
-	      float4_ts * pos_new,
-	      float4_ts * vel,
-	      int n):
-      p_pos_old(pos_old),
-      p_pos_new(pos_new),
-      p_vel(vel),
-      nparticle(n)
-   {}
+  functor_tbb( const float4_ts * pos_old,
+	       float4_ts * pos_new,
+	       float4_ts * vel,
+	       int n):
+    p_pos_old(pos_old),
+    p_pos_new(pos_new),
+    p_vel(vel),
+    nparticle(n)
+  {}
 
    
-   void operator()(const blocked_range<size_t> & r) const 
-   {
-      for ( size_t i = r.begin(); i != r.end(); ++i ) {
-	 update_particle(i, nparticle, p_pos_old, p_pos_new, p_vel);
-      }
-   }
+  void operator()(const blocked_range<size_t> & r) const 
+  {
+    for ( size_t i = r.begin(); i != r.end(); ++i ) {
+      update_particle(i, nparticle, p_pos_old, p_pos_new, p_vel);
+    }
+  }
 
 };
 
@@ -160,10 +160,8 @@ public:
 int main()
 {
   uint64_t        t0, t1, t2;
-  int nparticle = 2*8192; /* MUST be a nice power of two for simplicity */
-  const int nstep = 500;
-  //int nburst = 20; /* MUST divide the value of nstep without remainder */
-  //int nthread = 64; /* chosen for ATI Radeon HD 5870 */
+  size_t nparticle = 32; // MUST be a nice power of two for simplicity 
+  const int nstep = 10000;
 
   const float dt = 0.1;
   const float eps = 0.0001;
@@ -181,8 +179,9 @@ int main()
   
   const float4_t dt0(dt,dt,dt,0.0f);
 
+  srand(1232773);
   for ( int i = 0; i < nparticle; ++i ){
-    pos1.push_back(float4_t(0.f,0.f,0.f,0.f));
+    //pos1.push_back(float4_t(0.f,0.f,0.f,0.f));
     pos1[i].x = (float)rand()/RAND_MAX * 100. - 50.;
     pos1[i].y = (float)rand()/RAND_MAX * 100. - 50.;
     pos1[i].z = (float)rand()/RAND_MAX * 100. - 50.;
@@ -190,12 +189,17 @@ int main()
     pos2.push_back(float4_t(0.f,0.f,0.f,0.f));
     vel. push_back(float4_t(0.f,0.f,0.f,0.f));
   }
+  // dump
+  for ( size_t i = 0; i < nparticle; ++i ) {
+    printf("Initial: particle %zd x=%f, y=%f, z=%f, m=%f\n",
+	   i, pos1[i].x, pos1[i].y, pos1[i].z, pos1[i].w);
+  }
 
   int which = 8;
 
   float4_t startpos = pos1[which];
 
-  printf("Start: particle %d x=%f, y=%f, z=%f, m=%f\n",
+  printf("Start: particle %zd x=%f, y=%f, z=%f, m=%f\n",
 	 which, pos1[which].x, pos1[which].y, pos1[which].z, pos1[which].w);
   
   // start iterating
@@ -223,9 +227,9 @@ int main()
     //std::for_each(parts.begin(), parts.end(), functor_serial(pos_old, pos_new, &vel,nparticle));
 
     for( auto i: parts) 
-      update_particle(i,nparticle, pos_old, pos_new, &vel);    
-    // tbb::parallel_for(blocked_range<size_t>(0,nparticle),
-    //   		      functor_tbb(pos_old, pos_new, &vel,nparticle));
+      //update_particle(i,nparticle, pos_old, pos_new, &vel);    
+      tbb::parallel_for(blocked_range<size_t>(0,nparticle),
+			functor_tbb(pos_old, pos_new, &vel,nparticle));
 
 
 
@@ -253,7 +257,7 @@ int main()
   //dump
   for ( size_t i = 0; i < nparticle; ++i ) {
     printf("Final: particle %zd x=%f, y=%f, z=%f, m=%f\n",
-	 i, pos1[i].x, pos1[i].y, pos1[i].z, pos1[i].w);
+	   i, pos1[i].x, pos1[i].y, pos1[i].z, pos1[i].w);
   }
 
 
